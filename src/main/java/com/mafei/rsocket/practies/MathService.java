eb149d5370cbd48624f78b857a2a5d6b6652464b
mafei <mafei.dev@gmail.com>
2021-08-27 18:29:28 +0530

    fire and forget verify blocking the thread

--- src/main/java/com/mafei/rsocket/practies/MathService.java
+++ src/main/java/com/mafei/rsocket/practies/MathService.java
@@ -7,6 +7,12 @@ import reactor.core.publisher.Mono;
 public class MathService implements RSocket {
     @Override
     public Mono<Void> fireAndForget(Payload payload) {
+        try {
+            Thread.sleep(5000);
+            System.out.println("Thread > " + Thread.currentThread().getName());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         String data = payload.getDataUtf8();
         System.out.println("data = " + data);
         return Mono.empty();
